9e3cf8c0e697412d3f29c901b51fccbaf78c27da
Abhishek-web31 <abhishekrajputjbp@gmail.com>
2025-06-20 18:57:44 +0530

    Add files via upload

---
+++ campus_prep (1).sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2025 at 12:03 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `campus_prep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ai_questions`
+--
+-- Error reading structure for table campus_prep.ai_questions: #1932 - Table &#039;campus_prep.ai_questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.ai_questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`ai_questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `c_questions`
+--
+-- Error reading structure for table campus_prep.c_questions: #1932 - Table &#039;campus_prep.c_questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.c_questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`c_questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `general_questions`
+--
+-- Error reading structure for table campus_prep.general_questions: #1932 - Table &#039;campus_prep.general_questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.general_questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`general_questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oops_questions`
+--
+-- Error reading structure for table campus_prep.oops_questions: #1932 - Table &#039;campus_prep.oops_questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.oops_questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`oops_questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `python_questions`
+--
+-- Error reading structure for table campus_prep.python_questions: #1932 - Table &#039;campus_prep.python_questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.python_questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`python_questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+-- Error reading structure for table campus_prep.questions: #1932 - Table &#039;campus_prep.questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+-- Error reading structure for table campus_prep.results: #1932 - Table &#039;campus_prep.results&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.results: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`results`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study_references`
+--
+-- Error reading structure for table campus_prep.study_references: #1932 - Table &#039;campus_prep.study_references&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.study_references: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`study_references`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topic_week_allocation`
+--
+-- Error reading structure for table campus_prep.topic_week_allocation: #1932 - Table &#039;campus_prep.topic_week_allocation&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.topic_week_allocation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`topic_week_allocation`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table campus_prep.users: #1932 - Table &#039;campus_prep.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table campus_prep.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `campus_prep`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
